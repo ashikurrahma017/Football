e928435809e0677a11e7e842c96fa6c2123757f5
ashikurrahma017 <ashikurarefinistiak@gmail.com>
2026-04-29 17:49:41 +0530

    Refactor server.js with comments and dynamic port
    Refactor server.js to improve structure and add comments.

--- server.js
+++ server.js
@@ -3,35 +3,26 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+// Serve static files from public folder
 app.use(express.static("public"));
 
-let players = [];
-
-io.on("connection", (socket) => {
-
-    if (players.length < 2) {
-        players.push(socket.id);
-
-        const team = players.length === 1 ? "A" : "B";
-        socket.emit("team", team);
-
-        if (players.length === 2) {
-            io.emit("start");
-        }
-    }
-
-    socket.on("shoot", data => {
-        socket.broadcast.emit("shoot", data);
-    });
-
-    socket.on("goal", data => {
-        io.emit("goal", data);
-    });
-
-    socket.on("disconnect", () => {
-        players = players.filter(id => id !== socket.id);
-    });
-
+// Simple test route (optional)
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-http.listen(3000, () => console.log("Server running"));
+// Socket connection (for multiplayer later)
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+// IMPORTANT: Render uses dynamic port
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
